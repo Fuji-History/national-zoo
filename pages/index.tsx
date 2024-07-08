5ac02183eec5dc8e48c9d4f87ccdf8f4fb85702a
manjul <157203236+manjul-6969@users.noreply.github.com>
2024-07-08 16:23:28 +0000

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Manjul's Gallery</title>
+        <title>National Zoo</title>
         <meta
           property="og:image"
           content=""
@@ -59,10 +59,10 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
             {/* <Logo /> */}
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              All time photos
+              National Zoo Visit
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              All the crazy pictures mahfuz have taken so far...
+              Visit to the national zoo, mirpur, dhaka. 08-Jul-24
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
